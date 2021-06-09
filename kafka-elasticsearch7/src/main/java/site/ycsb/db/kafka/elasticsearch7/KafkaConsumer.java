957a81b7c8fc936216825d72d6b3eb1794358b1f
xosk31 <gonzalocalderonlillo@gmail.com>
2021-06-09 16:12:28 +0200

    Prepare project

---
+++ kafka-elasticsearch7/src/main/java/site/ycsb/db/kafka/elasticsearch7/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package site.ycsb.db.kafka.elasticsearch7;
+
+/**
+ * Elasticsearch 7.x Java High Level REST Client for YCSB framework.
+ * @author xosk31
+ * 
+ * <p>
+ * See {@code README.md} to configure and view more details.
+ * </p>
+ * 
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/client
+ *         /java-rest/master/java-rest-high.html">Elastic.co</a>
+ *
+ */
+public class KafkaConsumer {
+
+}
